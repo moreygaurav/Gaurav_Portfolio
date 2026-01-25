9654fd1daed2c8c55cc85476f359a325b73cfeb2
GauravMorey <gaurav1234pmorey@gmail.com>
2026-01-25 21:09:58 +0530

    fix the error and removed the import react from all the releted files as it is react 17 and about you dont need to import react maually

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import React from 'react';
 import './App.css'
 import AppRouter from './routes/AppRouter'
 
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,7 +1,4 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-
 import Home from "../Pages/Home";
 
 function AppRouter() {
