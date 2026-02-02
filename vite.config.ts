dedf86997b1669eff697122af4af3f696e036121
GauravMorey <gaurav1234pmorey@gmail.com>
2026-02-02 22:27:01 +0530

    removed base: /Gaurav_Portfolio/, from vite.config.ts as it is not needed to deploy project on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Gaurav_Portfolio/', // Must EXACTLY match your repo name
+  // base: '/Gaurav_Portfolio/', // Must EXACTLY match your repo name
   plugins: [react()],
 });
