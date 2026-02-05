69ca42d7c4425f4835e42d96d3cf1bef919785a9
GauravMorey <gaurav1234pmorey@gmail.com>
2026-02-05 21:26:43 +0530

    replaced the html structrue with the image based structure for GSAP animetions

--- src/components/LandingPage/landingPage.tsx
+++ src/components/LandingPage/landingPage.tsx
@@ -1,46 +1,45 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./landing.css";
+import "./landing.css"
 
 export default function Landing() {
   return (
     <section>
-      <div className="container-fluid">
-        <div className="row justify-content-center">
-          <div className="col-12 maindiv d-flex justify-content-center align-items-center mt-5">
+      <div className="bgdiv col-11 col-lg-11 mt-5 ">
+        {/* job roles */}
+        <img src="./img/JobRoles.svg" alt="Job Roles" className="JRImg  " />
 
-            {/* Image */}
-            <div className="limg p-0 mb-0">
-              <img
-                src="./img/Limg.png"
-                alt="Landing"
-                className="img-fluid position-lg-absolute z-2"
-              />
-            </div>
-   {/* text area 2 */}
-          <div className="text2 col-12 z-1 px-3 text-white text-center fw-light mx-2">
-            <p > REACT.JS | NODE.JS | JAVA | UI/UX - FIGMA | M.E.R.N STACK </p>
-          </div>
-            {/* Text */}
-            <div className="textarea col-12  text-center z-0 px-3">
-              <p className=" fw-bold ">
-                WELCOME
-              </p>
-            </div>
+        <img src="./img/Other.png" alt="Other img" className="Other  d-block" />
+        <img src="./img/Slogun.png" alt="Slogun img" className="Slogunimg" />
 
-          {/* text area 3 */}
+        <img src="./img/CREATIVE.png" alt="background img" className="BGImg" />
 
-          <div className="text3 d-flex col-12 col-lg-12 z-1 justify-content-between text-center align-content-center ">
-            <div className="name ">
-              <h1>Gaurav Morey</h1>
-            </div>
-            <div className="slogan ">
-              <h5>DESIGN THAT SPEAKS.</h5>
-              <h5>VALUES THAT CONVERT.</h5>
-            </div>
-          </div>
+        <img src="./img/HEROimg.png" alt="Landing img" className="HEROImg" />
 
-          </div>
-        </div>
+        <img src="./img/reactdev.png" alt="" className="reactdev " />
+
+        {/* Social media links  */}
+        {/* github link */}
+        <a href=" https://github.com/moreygaurav?tab=">
+          <img src="./img/github.png" alt="Github img" className="githubimg" />
+        </a>
+
+        {/* linkedin img */}
+        <a href="https://www.linkedin.com/in/gaurav-morey-272315243/ ">
+          <img src="./img/linkdin.png" alt="Github img" className="linkdimg" />
+        </a>
+
+        {/* facebook img */}
+        <a href="https://www.facebook.com/gsuravpradeemorey.morey/">
+          <img src="./img/facebook.png" alt="Github img" className="facebookimg" />
+        </a>
+        {/* instagram img */}
+        <a href="https://www.instagram.com/ign_gaurav/">
+          <img src="./img/instagram.png" alt="Github img" className="instagramimg" />
+        </a>
+        {/* whatsapp img */}
+        <a href="9359031017">
+          <img src="./img/whatsapp.png" alt="Github img" className="whatsappimg" />
+        </a>
       </div>
     </section>
   );
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './Navbar.css';
+import "./navbar.css"
 
 
 const Navbar: React.FC = () => {
